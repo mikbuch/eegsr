c9ff581390b5eb6e4c7ee86af93ee2a0910eb29d
mikbuch <mikolaj.buchwald@gmail.com>
2018-03-26 13:27:52 +0200

    Example improvement, time to create cores

--- examples/cca/cca_online_simulation.py
+++ examples/cca/cca_online_simulation.py
@@ -1,6 +1,7 @@
 from eegsr.dataset import fetch_example
-from eegsr.control import Manager
+from eegsr.preprocessing import FltRealTime
 from eegsr.decoding import CCA
+from eegsr.control import Manager
 
 '''
 Take samples from source file, filters it and classifies.
@@ -9,9 +10,14 @@ Take samples from source file, filters it and classifies.
 # Download example file and get it's location.
 source_file = fetch_example('cca_ssvep_decoding')
 
+# Define filter object.
+flt = FltRealTime(bandstop=(49, 51), bandpass=(1, 50))
+
+# Define CCA object.
 cca = CCA(ref_signals={'type': 'ideal', 'harmonics': 1})
 
-# If timecode set, the output will be written into file with current date.
-manager = Manager(source_file=source_file, cls=cca, record_data=True,
-                  output_file='timecode')
+# If timecode set, the output will be written into file with
+# current date.
+manager = Manager(filter=flt, cls=cca, source_file=source_file,
+                  record_data=True, output_file='timecode')
 manager.run()
